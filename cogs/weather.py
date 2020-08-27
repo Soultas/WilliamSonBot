48c6ceebb56c622ca4781c49a6fa54e81a30d85c
Soultas <ryangonzalesgme@gmail.com>
2020-08-26 18:16:27 -0700

    Added some commands and cogs

---
+++ cogs/weather.py
@@ -0,0 +1,47 @@
+import discord
+import json
+import requests
+
+from discord.ext import commands
+api_key="4cdbd2001df9931c5dcc30ab2b574f3b"
+
+base_url="https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&"
+
+print(base_url)
+
+class Weather(commands.cog):
+	def __init__(self,bot):
+		self.bot=bot
+	@client.command()
+	async def weather(self,ctx,*,city: str):
+		city_name=city
+		complete_url=base_url+"appid="+api_key+"&q="+city_name
+		response = requests.get(complete_url)
+		x = response.json()
+		channel = ctx.message.channel
+		if x['cod']!='404':
+			async with channel.typing():
+				y=x['main']
+				current_temp=y['temp']
+				current_temp_celcius = str(round(current_temp-273.15))
+				current_pressure=y['pressure']
+				current_humidity=y['humidity']
+				z=x['weather']
+				weather_description= z[0]['description']
+		
+			embed = discord.Embed(title=f"weather in {city_name}",
+			color=ctx.guild.me.top_role.color,
+			timestamp=ctx.message.created_at,)
+			embed.add_field(name="description",value=f"**{weather_description}**",inline=False)
+			embed.add_field(name="temperature(c)",value=f"**{current_temp_celcius}**°",inline=False)
+			embed.add_field(name="Humidity%",value=f"**{current_humidity}%**",inline=False)
+			embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**",inline=False)
+			embed.set_thumbnail(url="https://i.bb.co/cMrsxdX/weather.png")
+			embed.set_footer(text=f"Requested by {ctx.author.name}")
+
+			await channel.send(embed=embed)
+		else:
+			await.channel.send('city not found')
+
+def setup(bot):
+	bot.add_cog(weather(bot))
--- initial.py
+++ initial.py
@@ -1,10 +1,84 @@
 import discord
+import json
+import asyncio
+import time
+import aiohttp
+import subprocess
+import sys
+import requests
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='Dr ')
 
 class myClient(discord.Client):
 	async def on_ready(self):
 		print('Logged on as {0}!'.format(self.user))
 	async def on_message(self, message):
 		print('message from {0.author}: {0.content}'.format(message))
+async def on_ready(self):
+	print('Logged on as {0}!'.format(self.user))
+async def on_message(self, message):
+	print('message from {0.author}: {0.content}'.format(message))
 
-client = myClient()
-client.run('NzMwNTkxODQ0OTMyNzgwMDgy.XwZurQ.eJn9_b7SUJGujfVZhMskIPQqIps')
+Init_time = time.time()
+
+bot = commands.AutoShardedBot(command_prefix='Dr ', description="Dr Botman")
+
+
+@bot.command()
+async def shutdown(ctx):
+	await bot.logout()
+
+@bot.command()
+async def uptime(ctx):
+	second=time.time() - Init_time
+	minute, second = divmod(second, 60)
+	hour, minute = divmod(minute, 60)
+	day, hour = divmod(hour, 24)
+	week, day = divmod(day, 7)
+	await ctx.send("I've been online for %d weeks, %d days, %d hours, %d minutes, %d seconds" %(week,day,hour,minute,second))
+
+@bot.command()
+async def reload(ctx,extension: str):
+	extension = "commands.{}".format(extension)
+	await ctx.send('reloading {}..'.format(extension))
+	if extension in extension:
+		await ctx.send("reloading {}".format(extension))
+		bot.unload_extension(extension)
+		bot.load_extension(extension)
+		await ctx.send('{} reloaded'.format(extension))
+	else:
+		await ctx.send("Extension isn't available")
+
+@bot.command()
+async def disable(ctx,extension: str):
+	extension = "commands.{}".format(extension)
+	if extension in extension:
+		await ctx.send('Disabling {}'.format(extension))
+		bot.unload_extension(extension)
+		await ctx.send('{} disabled'.format(extension))
+	else:
+		await ctx.send("extension isn't available")
+
+@bot.command
+async def enable(ctx,extension: str):
+	extension = "commands.{}".format(extension)
+	if extension in extension:
+		await ctx.send("Loading {}..".format(extension))
+		try:
+			bot.load_extension(extension)
+		except:
+			await ctx.send(traceback.print_exc())
+		else:
+			await ctx.send('extenbsion not available')
+
+
+@bot.event
+async def on_ready():
+	print('Logged on as {0}!'.format(bot.user))
+	print('message from {0.author}: {0.content}'.format(bot.message))
+
+
+
+
+bot.run('NzMwNTkxODQ0OTMyNzgwMDgy.XwZurQ.qEnwWUjwaUiIyqaRhaxhp_bSODw')
